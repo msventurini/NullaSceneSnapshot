8480642b66b2262d276ec640614a2ad9f64915e1
msventurini <42521920+msventurini@users.noreply.github.com>
2024-11-19 21:09:35 -0300

    Initial Commit

---
+++ NullaSceneSnapshot/NullaSceneSnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  NullaSceneSnapshotApp.swift
+//  NullaSceneSnapshot
+//
+//  Created by Matheus Silveira Venturini on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NullaSceneSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
